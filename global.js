ccbc11874682697c23bd49b8220dd3a053e1ed2a
Tori Nguyen <ton010@ucsd.edu>
2025-10-23 22:53:59 -0700

    new changes

--- global.js
+++ global.js
@@ -6,14 +6,6 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-////////////////////////////// Step 2 /////////////////////////////////////////
-// navLinks = $$("nav a");
-// let currentLink = navLinks.find(
-//   (a) => a.host === location.host && a.pathname === location.pathname,
-// );
-// currentLink?.classList.add('current');
-
-
 ////////////////////////////// Step 3: Nav tag /////////////////////////////////
 let pages = [ // create array of dictionary for url link and label 
   { url: '', title: 'Home' },
@@ -82,4 +74,42 @@ select.addEventListener('input', function (event) {
   localStorage.colorScheme = event.target.value;
 });
 
+/////////////////////////////////////////////// Lab 4 /////////////////////////////////////////////
+// file to load project data from a JSON file and dynamically display it on the Projects page
+
+// in case url fail to get project url 
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    };
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+// create render func to dynamically generate and display project content
+// project object, containerElement where project will be displayed 
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // clear prior existing content in container
+  containerElement.innerHTML = '';
+
+  for (current in project) {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+    // Ensure containerElement is a valid DOM element in your tests
+    // expect(containerElement).toBeInstanceOf(HTMLElement);
+  }
+}
 
